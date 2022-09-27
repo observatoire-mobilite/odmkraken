bb6288fc75fe1037fee1992c0cf72e419341207e
Gil Georges <gil.georges@gmail.com>
2022-09-27 15:57:18 +0200

    Makes transform queries dynamic All transformations after initial import from CSV files was previously done through SQL functions. changing the name of the input table is rather inconvenient under plpgsql. So instead this new version switches to dynamic sql (using psycopg2.sql) from Python.

--- src/odmkraken/busspeeds/mapmatch.py
+++ src/odmkraken/busspeeds/mapmatch.py
@@ -9,7 +9,7 @@ from odmkraken.resources.edmo.busdata import VehicleTimeFrame
             config_schema={'date_from': str, 'date_to': str})
 def load_vehicle_timeframes(context: dagster.OpExecutionContext) -> typing.Iterator[dagster.DynamicOutput[VehicleTimeFrame]]:
     dates = context.op_config['date_from'], context.op_config['date_to']
-    for tf in  context.resources.edmo_bus_data.get_timeframes_on(*dates):
+    for tf in  context.resources.edmo_vehdata.get_timeframes_on(*dates):
         yield dagster.DynamicOutput(tf, mapping_key=str(tf.id.hex))
 
 
@@ -18,7 +18,7 @@ def most_likely_path(context: dagster.OpExecutionContext, vehicle_timeframe: Veh
 
     # lazy nearby road detection
     def nearby_roads(t: datetime, x: float, y: float):
-        roads = context.resources.edmo_bus_data.get_nearby_roads(t, x, y)
+        roads = context.resources.edmo_vehdata.get_nearby_roads(t, x, y)
         roads = [candidate_solution(*r, t) for r in roads]
         return roads
 
@@ -27,7 +27,7 @@ def most_likely_path(context: dagster.OpExecutionContext, vehicle_timeframe: Veh
     spe = context.resources.shortest_path_engine
 
     # reconstruct path over road segments
-    with context.resources.edmo_bus_data.get_pings(vehicle_timeframe) as cur:
+    with context.resources.edmo_vehdata.get_pings(vehicle_timeframe) as cur:
         path = reconstruct_optimal_path(cur, nearby_roads, shortest_path_engine=spe, scorer=scorer)
     
     # reproject in the perspective of the road segments
@@ -38,7 +38,7 @@ def most_likely_path(context: dagster.OpExecutionContext, vehicle_timeframe: Veh
 
 @dagster.op(required_resource_keys={'edmo_vehdata'})
 def extract_halts(context: dagster.OpExecutionContext, vehicle_timeframe: VehicleTimeFrame):
-    context.resources.edmo_bus_data.extract_halts(vehicle_timeframe)
+    context.resources.edmo_vehdata.extract_halts(vehicle_timeframe)
 
 
 @dagster.daily_partitioned_config(start_date=datetime(2020, 1, 1), hour_offset=4)
