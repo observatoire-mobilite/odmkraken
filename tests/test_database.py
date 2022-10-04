9b6925fa160362c26087743ed8fb892e28ad1fea
Gil Georges <gilgeorges@users.noreply.github.com>
2022-10-04 10:39:11 +0200

    Forcing psycopg2 to read env-vars

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,7 +3,7 @@ import psycopg2
 
 
 def test_db():
-  conn = psycopg2.connect()
+  conn = psycopg2.connect('')
   with conn.cursor() as cur:
     cur.execute('select version()')
     ver = cur.fetchone()
