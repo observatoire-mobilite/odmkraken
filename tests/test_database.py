720602ad9725d85507eb22074c0728e0b9aee65a
Gil Georges <gilgeorges@users.noreply.github.com>
2022-10-04 10:45:44 +0200

    Hardcoding the connection string

--- tests/test_database.py
+++ tests/test_database.py
@@ -3,7 +3,7 @@ import psycopg2
 
 
 def test_db():
-  conn = psycopg2.connect('')
+  conn = psycopg2.connect('postgres://postgres:postgres@localhost:5432')
   with conn.cursor() as cur:
     cur.execute('select version()')
     ver = cur.fetchone()
