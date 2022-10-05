a45694185852d80611ca1c65a37a73620074f92e
Gil Georges <ggeorges@doheem>
2022-10-05 17:20:51 +0200

    Changed: VehicleTimeFrame works with str too

--- src/odmkraken/resources/edmo/busdata/__init__.py
+++ src/odmkraken/resources/edmo/busdata/__init__.py
@@ -23,7 +23,8 @@ class VehicleTimeFrame:
     __slots__ = ('id', 'vehicle_id', 'time_from', 'time_to')
 
     def __post_init__(self):
-        self.id = uuid.UUID(self.id)
+        if not isinstance(self.id, uuid.UUID):
+            self.id = uuid.UUID(self.id)
 
     def flat(self) -> typing.Tuple[int, datetime, datetime]:
         return (self.vehicle_id, self.time_from, self.time_to)
