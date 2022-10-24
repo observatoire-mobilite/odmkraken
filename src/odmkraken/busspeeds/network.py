af8bd50be4060cb7db706ca37843db9c43979bc5
Gil Georges <gilgeorges@users.noreply.github.com>
2022-10-24 17:25:50 +0200

    Fixed: allowing load_network to use zipfiles

--- src/odmkraken/busspeeds/network.py
+++ src/odmkraken/busspeeds/network.py
@@ -1,5 +1,6 @@
 """Load network description."""
 import typing
+import zipfile
 import dagster
 
 
@@ -13,7 +14,7 @@ def load_nodes(context: dagster.OpExecutionContext):
     """Load nodes file."""
     context.log.info('reading nodes file ...')
     table = ('network', 'road_nodes')
-    with open(context.op_config['network_node_file'], 'r', encoding='utf8') as buffer:
+    with open_file(context.op_config['network_node_file'], 'r', encoding='utf8') as buffer:
         context.resources.local_postgres.copy_from(buffer, table)
 
 
@@ -27,7 +28,7 @@ def load_edges(context: dagster.OpExecutionContext):
     """Load edges file."""
     context.log.info('reading edges file ...')
     table = ('network', 'road_edges')
-    with open(context.op_config['network_edge_file'], 'r', encoding='utf8') as buffer:
+    with open_file(context.op_config['network_edge_file'], 'r', encoding='utf8') as buffer:
         context.resources.local_postgres.copy_from(buffer, table)
 
 
@@ -36,3 +37,13 @@ def load_network():
     """Load complete network model."""
     load_nodes()
     load_edges()
+
+
+def open_file(file: pathlib.Path, *args, **kwargs) -> typing.IO:
+    if not zipfile.is_zipfile(file):
+        return file.open('rb')
+
+    archive = zipfile.ZipFile(file)
+    if len(archive.filelist) != 1:
+        raise RuntimeError('zip-file has zero or more than one entry')
+    return archive.open(archive.filelist[0], *args, **kwargs)
