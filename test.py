73cc09109b598582f9782c7020e1dfb58667d97b
Gil Georges <ggeorges@doheem>
2022-12-22 12:46:57 +0100

    Sync: wip

--- test.py
+++ test.py
@@ -2,52 +2,74 @@ import multiprocessing as mp
 from queue import Empty
 import time
 import dagster
+import uuid
+import random
+import collections
+import contextlib
 
 
-def subscriber(jobs: mp.Queue, results: mp.Queue):
+def subscriber(jobs: mp.Queue, done: mp.Queue):
     while True:
         try:
-            job = jobs.get(timeout=3)            
+            job = jobs.get(timeout=1)
         except Empty:
             print('no more work')
             break
         
-        print(f'received {job}', flush=True)
-
         # process job
-        time.sleep(1)
+        t = random.random() * 0.1
+        time.sleep(t)
         
-        print(f'done with {job}', flush=True)
-        results.put(job + 100)
+        done.put((mp.current_process().pid, t), timeout=10)
+
+    print('exiting')
+    return
+
+
+class ResultGatherer:
+
+    def __init__(self):
+        self.results = []
+
+    def count(self) -> collections.Counter:
+        time = {}
+        for r in self.results:
+            time[r[0]] = time.get(r[0], 0) + r[1]
+        return time
+
+    def gather(self, results):
+        while True:
+            try:
+                res = results.get_nowait()
+                self.results.append(res)
+            except Empty:
+                break
+            
 
 
 def publisher():
     
     # set up communication objects
-    jobs = mp.Queue(maxsize=5)
-    results = mp.Queue(maxsize=10)
+    jobs = mp.Queue(maxsize=10)
+    done = mp.Queue()
     
     # start worker pool
-    processes = [mp.Process(target=subscriber, args=(jobs, results )) for i in range(3)]
+    processes = [mp.Process(target=subscriber, args=(jobs, done )) for i in range(3)]
     for p in processes:
         p.start()
 
-    # send jobs
-    for i in range(20):
-        print(f'sending {i}')
-        jobs.put(i)
-
-    while True:
-        try:
-            r = results.get(timeout=0.3)
-            print(f'received result: {r}')
-        except Empty:
-            break
+    results = ResultGatherer()
+    pending_jobs = iter(uuid.uuid4() for i in range(1500))
+    for job in pending_jobs:
+        results.gather(done)
+        jobs.put(job)
     
     print('no more jobs')
+    results.gather(done)
     for p in processes:
         p.join()
-    
+    print(results.count())
+
 
 if __name__ == '__main__':
     publisher()
