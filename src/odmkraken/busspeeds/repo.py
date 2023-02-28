a5a1a66b9dec946890c350deb48c0a6a3921caaf
Gil Georges <gilgeorges@users.noreply.github.com>
2023-02-28 15:45:22 +0100

    Fix: a job to run on github live-tests.yaml

--- src/odmkraken/busspeeds/repo.py
+++ src/odmkraken/busspeeds/repo.py
@@ -1,16 +1,12 @@
 import typing
 import dagster
-#from .mapmatch import mapmatch_bus_data, mapmatch_config
 from .mapmatcher import most_likely_path
-from odmkraken.busspeeds import extract  #, mapmatch
+from odmkraken.busspeeds import extract
+from odmkraken.busspeeds.common import busdata_partition
 from .network import load_network
 from odmkraken.resources import RESOURCES
 from odmkraken.resources.pandas import pandas_parquet_manager, icts_data_manager
 
-
-local_icts_data_manager = icts_data_manager.configured({
-    'base_path': '/Users/ggeorges/Documents/staat/bus_data'
-})
 
 
 @dagster.repository
@@ -21,8 +17,9 @@ def busspeeds():
                 *dagster.load_assets_from_modules([extract]), 
                 most_likely_path
             ],
-            resource_defs={'icts_data_manager': local_icts_data_manager,
+            resource_defs={'icts_data_manager': icts_data_manager,
                            'pandas_data_manager': pandas_parquet_manager}
         ),
+        dagster.define_asset_job(name='process_busdata', selection=['duplicate_pings', 'pings', 'pings_from_stops', 'runs'], partitions_def=busdata_partition),
         load_network.to_job(resource_defs=RESOURCES)
     ]
